14a6c006c5c8b14d78351931d8d586dd6883e2fa
zer0def <zer0def@zer0def.0>
2023-11-16 10:21:45 +0100

    wip: hack: blunt impls of missing interfaces

--- pkg/server/kv.go
+++ pkg/server/kv.go
@@ -96,11 +96,19 @@ func toKV(kv *KeyValue) *mvccpb.KeyValue {
 }
 
 func (k *KVServerBridge) Put(ctx context.Context, r *etcdserverpb.PutRequest) (*etcdserverpb.PutResponse, error) {
-	return nil, unsupported("put")
+	res, err := k.limited.Put(ctx, r)
+	if err != nil {
+		logrus.Errorf("error in put %s: %v", r, err)
+	}
+	return res, err
 }
 
 func (k *KVServerBridge) DeleteRange(ctx context.Context, r *etcdserverpb.DeleteRangeRequest) (*etcdserverpb.DeleteRangeResponse, error) {
-	return nil, unsupported("delete")
+	res, err := k.limited.DeleteRange(ctx, r)
+	if err != nil {
+		logrus.Errorf("error in deleteRange %s: %v", r, err)
+	}
+	return res, err
 }
 
 func (k *KVServerBridge) Txn(ctx context.Context, r *etcdserverpb.TxnRequest) (*etcdserverpb.TxnResponse, error) {
